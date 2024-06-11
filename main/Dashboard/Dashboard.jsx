357e35d07e99188059d3babcd2ac0058cd13d454
Latte123456 <inayumi7@gmail.com>
2024-06-11 12:36:12 +1200

    Dashboard update
    - Respective folders have been created

---
+++ main/Dashboard/Dashboard.jsx
@@ -0,0 +1,14 @@
+import { userState } from 'react'
+import './Dashboard.css'
+
+function Dashboard()
+{
+    return
+    (
+        <div className='grid-container'>
+            <Sidebar />
+        </div>
+    )
+}
+
+export default Dashboard
